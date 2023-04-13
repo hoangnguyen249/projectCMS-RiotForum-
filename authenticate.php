77ef7639f9acf5c2602dcf731983fbbd989d3843
hoangnguyen249 <127330662+hoangnguyen249@users.noreply.github.com>
2023-04-12 20:54:11 -0500

    Add files via upload
    Add code to check for admin and user

---
+++ authenticate.php
@@ -0,0 +1,78 @@
+<?php
+
+require('connect.php');
+
+function test_input($data) {
+	
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	
+	$username = test_input($_POST['username']);
+	$password = test_input($_POST['password']);
+	$stmt = $db->prepare("SELECT * FROM adminlogin");
+	$stmt->execute();
+	$users = $stmt->fetchAll();
+	
+	foreach($users as $user) {
+		
+		if(($user['username'] == $username) &&
+			($user['password'] == $password)) {
+				header("location: adminpage.php");
+		}
+	}
+    echo "<script language='javascript'>";
+    echo "alert('WRONG INFORMATION')";
+    echo "</script>";
+   
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+ 
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href=
+"https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="login.css">
+    <title>Login Page</title>
+</head>
+ 
+<body>
+    <form action="authenticate.php" method="post">
+        <div class="login-box">
+            <h1>Login</h1>
+ 
+            <div class="textbox">
+                <i class="fa fa-user" aria-hidden="true"></i>
+                <input type="text" placeholder="Username"
+                         name="username" value="">
+            </div>
+ 
+            <div class="textbox">
+                <i class="fa fa-lock" aria-hidden="true"></i>
+                <input type="password" placeholder="Password"
+                         name="password" value="">
+            </div>
+ 
+            <input class="button" type="submit"
+                     name="login" value="Sign In">
+        </div>
+    </form>
+    <footer>
+        <p><a href="non-userpage.php">Guest</a></p>
+</footer>
+</body>
+ 
+</html>
+
+
