bdbe6504bae811e7e23c4bb774183658b3f9e32a
hoangnguyen249 <127330662+hoangnguyen249@users.noreply.github.com>
2023-04-12 20:59:36 -0500

    Add files via upload
    add category and css file

---
+++ category.php
@@ -0,0 +1,134 @@
+<?php
+require('connect.php');
+
+
+if(isset($_POST)){
+
+    if(isset($_POST['add_category']) && trim($_POST['new_category']) != '' ){
+        add_category();
+        header("location: adminpage.php"); // should go to user page once formed
+    }
+
+    if(isset($_POST['rename_category']) && trim($_POST['rename']) != '' ){
+        rename_category();
+        header("location: adminpage.php");// should go to user page once formed
+    }
+
+    if(isset($_POST['delete_category'])){
+        remove_category();
+        header("location: adminpage.php"); // should go to user page once formed
+    }
+}
+
+//add category
+function add_category(){
+
+    global $db;
+    //sanitize the data
+    $category = filter_input(INPUT_POST, 'new_category', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    $query = "INSERT INTO categories(category_name) VALUES (:category_name);";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':category_name', $category);
+
+    if($statement->execute()){
+        echo "Success";
+    }
+}
+
+//removve catgory
+function remove_category(){
+    global $db;
+
+    $category_id = filter_input(INPUT_POST, 'loaded_category', FILTER_SANITIZE_NUMBER_INT);
+
+    $query = "DELETE FROM categories WHERE category_id= :id";
+
+    $statement= $db->prepare($query);
+    $statement->bindValue(':id', $category_id, PDO::PARAM_INT);
+    if($statement->execute()){
+        echo("Success");
+
+    } // add an alert to confirm delete
+}
+
+// rename category
+function rename_category(){
+    global $db;
+    //sanitize the data
+    $category = filter_input(INPUT_POST, 'rename', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $category_id = filter_input(INPUT_POST, 'loaded_category', FILTER_SANITIZE_NUMBER_INT);
+
+    $query = "UPDATE categories SET category_name = :category_name WHERE category_id= :category_id;";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':category_name', $category);
+    $statement->bindValue(':category_id', $category_id);
+
+    if($statement->execute()){
+        echo "Success";
+    }
+
+}
+
+//loading comment
+function loading_categories(){
+    global $db;
+
+    $query = "SELECT * FROM categories ;";
+        // preparring sql for executoin
+    $statement = $db->prepare($query);
+    
+        //executing sql
+    $statement->execute();
+    $categories = [];
+    while ($x = $statement->fetch() ){
+        $categories[] = $x;
+        
+    }
+    
+    return $categories;
+}
+$row = loading_categories();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>My Blog Post!</title>
+</head>
+<body>
+    <!-- loading the categories--->
+    <form action="category.php" method="post">
+
+    <label for="loaded_category">Category</label>
+    <select name="loaded_category" >
+
+    <?php foreach($row as $category_type): ?>    
+    <option value="<?= $category_type['category_id'] ?>"> <?= $category_type['category_name'] ?> </option>
+    <?php endforeach ?>
+
+    </select>
+   
+
+    <label for="rename">Rename</label>
+    <input type="text" name="rename">
+
+    <input type="submit" value="Rename Category" name="rename_category">
+    <input type="submit" value="Delete Category" name="delete_category">
+    </form>
+
+
+    <!-- adding new categories--->
+    <form action="category.php" method="post">
+        <label for="new_category">Category</label>
+        <input type="text" name="new_category">
+
+        <input type="submit" value="Add Category" name="add_category">
+        
+    </form>
+</body>
+</html>
---
+++ category_post.php
@@ -0,0 +1,86 @@
+<?php
+
+
+
+bhhh
+
+require('connect.php');
+$query = "SELECT * FROM Post ORDER BY post_id ASC";
+
+$statement = $db->prepare($query);
+$statement->execute();
+function loading_categories(){
+    global $db;
+
+    $query = "SELECT * FROM categories ;";
+        // preparring sql for executoin
+    $statement = $db->prepare($query);
+    
+        //executing sql
+    $statement->execute();
+    $categories = [];
+    while ($x = $statement->fetch() ){
+        $categories[] = $x;
+        
+    }
+
+
+    
+    return $categories;
+}
+$row = loading_categories();
+
+if(isset($_POST['submit'])){
+    $query = "SELECT * FROM Post WHERE category = :id";
+
+    $statement = $db->prepare($query);
+    $statement->bindValue(':id', $_POST['category']);
+    $statement->execute();
+
+}
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>Welcome to my Blog!</title>
+</head>
+<body>
+    <!-- Remember that alternative syntax is good and html inside php is bad -->
+<p><a href= "adminpage.php">Go back to home page</a></p>
+
+        <div id=post>
+        
+        <form id="post" action="category_post.php" method="post">
+        <select name="category">
+            <?php foreach($row as $category_type): ?>
+                <option value="<?= $category_type['category_id'] ?>"> <?= $category_type['category_name'] ?> </option>
+            <?php endforeach ?>
+        </select>
+        <input type="submit" name="submit" value="Pick">
+            </form>
+           
+
+            <?php while ($row= $statement->fetch()): ?>
+                <h2><?= $row['title'] ?></h2>
+                <h4><?= $row['champion']?></h4>
+                <p><?= $row['date']?></p>
+                <p><?= $row['content'] . " " ?></p>
+                <p><b>Created:</b> <?= $row['created_date']?> <b>Updated:</b><?= $row['updated_date']?></p>
+                <br>
+           
+            <?php endwhile ?>
+        </div>
+
+            
+</body>
+</html>
